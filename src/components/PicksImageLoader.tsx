d81b55a4f84aa0e1a159e8168dcc65f1687acad8
divyanshchahar <divyanshchahar@outlook.com>
2022-10-17 12:27:17 +0530

    added functionality to load multiple images

---
+++ src/components/PicksImageLoader.tsx
@@ -0,0 +1,39 @@
+import ImageIndex from "../assets/images/ImageIndex";
+
+type AppProp = {
+  prop: string[];
+};
+
+/**
+ * Uploads Images of crypto assets
+ *
+ * @param prop Array of crypto asset ids
+ * @returns Images of crypto assets
+ */
+
+function PicksImageLoader(prop: AppProp) {
+  const picksImages: any[] = [];
+  const { prop: picks } = prop;
+  picks.forEach((picksItem) => {
+    ImageIndex.map((imageItem) => {
+      if (imageItem.id === picksItem) {
+        picksImages.push(imageItem.imageUrl);
+      }
+    });
+  });
+  return (
+    <div className="picks-container">
+      {picksImages.map((item) => {
+        return (
+          <img
+            className="picks"
+            src={item}
+            alt="coin to be buy with this trade"
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default PicksImageLoader;
