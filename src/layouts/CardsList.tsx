e63d0a1da235197b9b0fd0da0b1f904d28c7a4d3
divyanshchahar <divyanshchahar@outlook.com>
2022-10-17 12:34:50 +0530

    added functionality to render cards list

---
+++ src/layouts/CardsList.tsx
@@ -0,0 +1,32 @@
+// IMPORTING COMPONENTS
+import Card from "./Card";
+
+// IMPORTING ASSETS
+import trading_data from "../assets/data/trading_data.json";
+
+/**
+ * Renders cards
+ *
+ * @returns
+ */
+
+function CardsList() {
+  return (
+    <div>
+      {trading_data.map((item) => {
+        return (
+          <Card
+            id={item.id}
+            name={item.name}
+            price={item.price}
+            change={item.change}
+            tvl={item.tvl}
+            picks={item.picks}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default CardsList;
